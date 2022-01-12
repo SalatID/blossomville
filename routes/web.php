2f306292e52894b83315cfe0c6d6630b9d7de867
SalatID <31203124+SalatID@users.noreply.github.com>
2022-01-12 23:24:32 +0700

    upload template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::prefix('dev')->group(function () {
+    Route::get('/',[GuestController::class,'index']);
 });
