f41ba708d65e8c8d257382fa7ce68084d26fa878
SalatID <31203124+SalatID@users.noreply.github.com>
2022-01-26 13:37:13 +0700

    Revert "Revert "landing page to db""
    This reverts commit 83dcd7cfa674fee44bfe92946231d7a0e612f361.

---
+++ database/migrations/2022_01_26_030000_create_product_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('product_name',100);
+            $table->float('price');
+            $table->text('description');
+            $table->string('active',1);
+            $table->float('stock');
+            $table->string('image',150);
+            $table->integer('created_user');
+            $table->integer('updated_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
